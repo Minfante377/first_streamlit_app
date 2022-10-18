dfc1c761043bf4d85dc16f96b064588cd3c899ca
Martin Infante <maleinf09@gmail.com>
2022-10-18 10:12:07 -0300

    Save index change

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,7 @@ streamlit.text("Hard-Boiled Free-Range Egg")
 streamlit.header("Build your own Fruit Smoothie")
 
 my_fruit_df = pd.read_csv(FRUIT_LIST_PATH)
-my_fruit_df.set_index("Fruit")
+my_fruit_df = my_fruit_df.set_index("Fruit")
 
 streamlit.multiselect("Pick some fruits:", list(my_fruit_df.index))
 streamlit.dataframe(my_fruit_df)
